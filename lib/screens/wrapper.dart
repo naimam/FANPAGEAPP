5e3fb695426a01724cb91677a17c0caa6fc3cb53
Naima <naimamohamed19@hotmail.com>
2022-02-18 19:41:31 -0500

    creating app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:fanpage_app/screens/authenticate/authenticate.dart';
+import 'package:fanpage_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return either home or auth widget
+    return Authenticate();
+  }
+}
